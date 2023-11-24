045238b2e2137e0d2508dd123b14b09112b2e43f
engeneee <zhuldyzai.rashid@jihc.kz>
2023-11-24 11:15:37 +0600

    TravelApp

---
+++ lib/pages/navpages/bar_item_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:travel_app/pages/navpages/my_page.dart';
+
+class BarItemPage extends StatelessWidget {
+  const BarItemPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Container(
+      child: Center(
+        child: Text(
+        "Bar Item Page"),
+      ));
+  }}
